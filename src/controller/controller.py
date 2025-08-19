49df2785ef9b6f997b942dc9f814333b7844f68e
Zachary Gonzalez <146303521+ZacharyGonzalez@users.noreply.github.com>
2025-08-19 14:20:32 -0400

    Update controller.py

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,6 +1,4 @@
 """
-
-If i add RLE
-
-along with my other ideas, this controller will be the wrapper that uses the handlers on the bitstream
+The current idea is to have this act as the shot caller to facilitate the generation of unique process-keys, which is the end goal of this pseudo compressor. 
+A key to use for recompilation of the binary data 
 """
